8bcd0fa5b543b934ac3d93fab9eb243f0480081d
tenkco <sytnikkv@gmail.com>
2025-11-17 21:00:05 +0700

    fix

--- desing_pro/urls.py
+++ desing_pro/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/applications/', permanent=True)),
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
     path('superadmin/', admin.site.urls, name='admin'),
     path('catalog/', include('catalog.urls')),
 ]
