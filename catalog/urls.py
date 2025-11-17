d13142df0a7b6c3ef90897d98857172280e4b5a3
tenkco <sytnikkv@gmail.com>
2025-11-17 17:22:56 +0700

    create application

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,7 +4,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register', views.Register.as_view(), name='register'),
+    path('register/', views.Register.as_view(), name='register'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('create_application/', views.Application.as_view(), name='create_applicattion'),
 ]
--- desing_pro/urls.py
+++ desing_pro/urls.py
@@ -1,10 +1,13 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+    path('', RedirectView.as_view(url='/applications/', permanent=True)),
     path('superadmin/', admin.site.urls, name='admin'),
     path('catalog/', include('catalog.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
